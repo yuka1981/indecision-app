883128f7979af71a573faaa927551ed8462a89df
Reid <touyalin@gmail.com>
2023-06-03 23:41:57 +0800

    3.11 JSX expressions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,38 @@
 "use strict";
 
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the handle of a computer"
+};
+
 var template = React.createElement(
-  "p",
+  "div",
   null,
-  "This is a template!"
+  React.createElement(
+    "h1",
+    null,
+    app.title
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.subtitle
+  )
 );
+
+// var user = {
+//   name: "Reid",
+//   age: 41,
+//   location: "Taipei",
+// };
+//
+// var template = (
+//   <div>
+//     <h1>{user.name}</h1>
+//     <p>Age: {user.age}</p>
+//     <p>Location: {user.location}</p>
+//   </div>
+// );
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,4 +1,28 @@
-var template = <p>This is a template!</p>;
+var app = {
+  title: "Indecision App",
+  subtitle: "Put your life in the handle of a computer",
+};
+
+var template = (
+  <div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+  </div>
+);
+
+// var user = {
+//   name: "Reid",
+//   age: 41,
+//   location: "Taipei",
+// };
+//
+// var template = (
+//   <div>
+//     <h1>{user.name}</h1>
+//     <p>Age: {user.age}</p>
+//     <p>Location: {user.location}</p>
+//   </div>
+// );
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
