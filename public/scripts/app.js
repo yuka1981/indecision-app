31670ff73d63be01911232136a4b12244c1d2713
Reid <touyalin@gmail.com>
2023-06-04 00:15:47 +0800

    3.12 condition rendering in JSX

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,7 +2,8 @@
 
 var app = {
   title: "Indecision App",
-  subtitle: "Put your life in the handle of a computer"
+  subtitle: "Put your life in the handle of a computer",
+  options: ["One", "Two"]
 };
 
 var template = React.createElement(
@@ -13,26 +14,18 @@ var template = React.createElement(
     null,
     app.title
   ),
-  React.createElement(
+  app.subtitle && React.createElement(
     "p",
     null,
     app.subtitle
+  ),
+  React.createElement(
+    "p",
+    null,
+    app.options.length > 0 ? "Here are your options" : "No options"
   )
 );
 
-// var user = {
-//   name: "Reid",
-//   age: 41,
-//   location: "Taipei",
-// };
-//
-// var template = (
-//   <div>
-//     <h1>{user.name}</h1>
-//     <p>Age: {user.age}</p>
-//     <p>Location: {user.location}</p>
-//   </div>
-// );
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,28 +1,17 @@
 var app = {
   title: "Indecision App",
   subtitle: "Put your life in the handle of a computer",
+  options: ["One", "Two"],
 };
 
 var template = (
   <div>
     <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
+    {app.subtitle && <p>{app.subtitle}</p>}
+    <p>{app.options.length > 0 ? "Here are your options" : "No options"}</p>
   </div>
 );
 
-// var user = {
-//   name: "Reid",
-//   age: 41,
-//   location: "Taipei",
-// };
-//
-// var template = (
-//   <div>
-//     <h1>{user.name}</h1>
-//     <p>Age: {user.age}</p>
-//     <p>Location: {user.location}</p>
-//   </div>
-// );
 var appRoot = document.getElementById("app");
 
 ReactDOM.render(template, appRoot);
