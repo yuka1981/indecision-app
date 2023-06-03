49f73d08930285838bc3472ca638f092b56a8059
Reid <touyalin@gmail.com>
2023-06-03 22:47:51 +0800

    3.9 setting up babel

---
+++ public/scripts/app.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var template = React.createElement(
+  "p",
+  null,
+  "This is a template!"
+);
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,4 @@
+var template = <p>This is a template!</p>;
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
