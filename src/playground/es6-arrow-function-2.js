62dc1d52a9e44f33ea7ddb8fef10482b7231c13e
Reid <touyalin@gmail.com>
2023-06-04 17:18:55 +0800

    3.15 arrow function part 2

--- src/playground/es6-arrow-function-2.js
+++ src/playground/es6-arrow-function-2.js
@@ -1,20 +1,14 @@
-"use strict";
-
 // Challenge area
 
-var multiplier = {
+const multiplier = {
   // number - array of numbers
   // multiplyBy - single number
   // multiply - return a new array where the number have been multiplied
   numbers: [1, 2, 3],
   multiplyBy: 3,
-  multiply: function multiply() {
-    var _this = this;
-
-    return this.numbers.map(function (number) {
-      return number * _this.multiplyBy;
-    });
-  }
+  multiply() {
+    return this.numbers.map((number) => number * this.multiplyBy);
+  },
 };
 
 console.log(multiplier.multiply());
