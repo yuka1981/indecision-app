1f046f29587486153c7816e1150da2ae86d5f9a1
Reid <touyalin@gmail.com>
2023-06-18 22:32:34 +0800

    6.52 es6 import/export

--- src/app.js
+++ src/app.js
@@ -1,1 +1,15 @@
-console.log("app.js is running!!!");
+// import { square, add } from "./utils.js";
+//
+// console.log("app.js is running!!!");
+// console.log(square(4));
+// console.log(add(100, 23));
+
+// person
+// named export isAdult(18) - true if adult, otherwise false
+// named export canDrink(18) - true if 21 and over, otherwise false
+import { isAdult, canDrink } from "./person.js";
+
+console.log("age = 18", isAdult(18));
+console.log("age = 16", isAdult(16));
+console.log("age = 21", isAdult(21));
+console.log("age = 18", canDrink(18));
---
+++ src/utils.js
@@ -0,0 +1,8 @@
+console.log("utils is running");
+
+export const square = (x) => x * x;
+
+export const add = (x, y) => x + y;
+// export - default export - named export
+// named export
+// export { square, add };
